6c36372a2a835de26dc7f80c86ef75f3ca386003
Rodolfo Galván Martínez <mixcoatl@mixco.lan>
2022-10-01 23:58:49 -0500

    Added driver for reverse string.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import com.mixco.p1.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ReverseString p1 = new ReverseString();
+        Scanner p1Input = new Scanner(System.in);
+        System.out.print("Enter a string to reverse: ");
+        String strToReverse = p1Input.nextLine();
+        System.out.println(p1.reverse(strToReverse));
+        System.out.println(" ");
     }
 }
